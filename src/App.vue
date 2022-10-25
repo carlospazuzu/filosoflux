f824908477761074eca32ae3276f5270575f975c
carlospazuzu <carlosh555@hotmail.com>
2022-10-25 02:39:18 -0300

    main logic finished, just need to manually add all subject entries and their respective pre-requisities

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-2">
+        <Subject subject="Seminário de Introd. ao Curso de Fil." carga="15" cr="01" sId="seminario" />
+        <Subject subject="Iniciação à Fil. e à Red. Filosófica" carga="75" cr="05" sId="iniciacao" ref="iniciacao" />
+        <Subject subject="Introdução à Met. Científica (IMC)" carga="60" cr="04" sId="imc" />
+        <Subject subject="Sociologia da Educação" carga="60" cr="04" sId="sociologiaeduc" />
+        <Subject subject="Inglês Instr. I" carga="60" cr="04" sId="inglesfrances" />
+        <Subject subject="História da Educação" carga="60" cr="04" sId="histeduc" />
+      </div>
+      <div class="col-2">
+        <Subject subject="História da Filosofia Antiga" carga="75" cr="05" sId="filosantiga" pre="iniciacao"/>
+        <Subject subject="Lógica I" carga="60" cr="04" sId="logica1" pre="iniciacao"/>
+        <Subject subject="Ontologia I" carga="60" cr="04" sId="ontologia1" pre="iniciacao"/>
+        <Subject subject="Teoria do Conhec. I" carga="60" cr="04" sId="teoriaconhecimento1" pre="iniciacao"/>
+        <Subject subject="Psicologia da Educação" carga="60" cr="04" sId="psicoeduc" />
+        <Subject subject="Seminário em Fil. e Meio Ambiente" carga="15" cr="01" sId="semfilmeioamb" />
+      </div>
+      <div class="col-2">
+        <Subject subject="História da Filosofia Medieval" carga="75" cr="05" sId="filosmedieval" pre="filosantiga"/>
+        <Subject subject="Tóp. Esp. em His. da Fil. Antiga" carga="30" cr="02" sId="topespfilantiga" pre="filosantiga"/>
+        <Subject subject="Ontologia II" carga="60" cr="04" sId="ontologia2" pre="ontologia1"/>
+        <Subject subject="Teoria do Conhec. II" carga="60" cr="04" sId="teoriaconhecimento2" pre="teoriaconhecimento1,logica1"/>
+        <Subject subject="Introdução à Ética" carga="60" cr="04" sId="intetica" pre="ontologia1,filosantiga,logica1"/>
+        <Subject subject="Legislação e Org. da Educ. Básica" carga="60" cr="04" sId="legisl"/>
+        <Subject subject="Ativ. Complem." carga="60" cr=" " sId="ativcomp"/>
+      </div>
+      <div class="col-2">
+        <Subject subject="História da Filosofia Moderna" carga="90" cr="06" sId="filosmoderna" pre="filosmedieval"/>
+        <Subject subject="Teorias da Ética" carga="75" cr="05" sId="teoriasetica" pre="intetica,teoriaconhecimento2,filosmedieval,ontologia2"/>
+        <Subject subject="Antropologia Filosófica I" carga="60" cr="04" sId="antfilos" pre="ontologia1,teoriaconhecimento1"/>
+        <Subject subject="Avaliação de Aprendizagem" carga="60" cr="04" sId="avaliacao" oferta="true" />
+        <Subject subject="Seminário em Filosofia (Optativa)" carga="30" cr="02" sId="seminariofilos" oferta="true" />
+        <Subject subject="Didática Geral" carga="60" cr="04" sId="didatica" pre="imc" />
+        <Subject subject="Ativ. Complem." carga="40" cr=" " sId="ativcomp"/>
+      </div>
+      <div class="col-2">
+        <Subject subject="História da Filosofia Contemp. I" carga="60" cr="04" sId="filoscontemp1" pre="filosmoderna"/>
+        <Subject subject="Metodologia Filosófica" carga="75" cr="05" sId="metfilosofica" pre="filosmoderna,teoriaconhecimento2,logica1"/>
+        <Subject subject="Tóp. Esp. em His. da Fil. Moderna" carga="30" cr="02" sId="topespfilmoderna" pre="filosmoderna"/>
+        <Subject subject="Metodologia do Ens. de Filosofia" carga="60" cr="04" sId="metensfilos" pre="didatica,filosmoderna,ontologia2,teoriasetica"/>
+      </div>
+      <div class="col-2">
+        <Subject subject="Filosofia da Educação" carga="60" cr="04" sId="filoseduc" pre="filosmoderna"/>
+      </div>
+      <div class="col-2"></div>
+      <div class="col-2"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Subject from './components/Subject.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {    
+    Subject
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Subject.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <table :id="sId" @mouseover="doHighligh" @mouseleave="ceaseHighligh" :class="{main: hovering, oferta: this.oferta != null && hovering}">
+            <tr>
+                <td class="subject" rowspan="3">{{subject}}</td>
+                <td class="carga" rowspan="2">{{carga}}</td>
+            </tr>
+            <tr>       
+            </tr>
+            <tr>
+                <td class="cr">{{cr}}</td>
+            </tr>    
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Subject',
+    props: ['subject', 'carga', 'cr', 'sId', 'pre', 'oferta'],
+    methods: {
+      doHighligh() {
+        this.hovering = true;
+        const preRequisites = this.pre.split(',');
+        preRequisites.map( (subject) => document.getElementById(subject).classList.add('pre'));
+        
+        // this.pre.
+      },
+      ceaseHighligh() {
+        this.hovering = false;
+        const preRequisites = this.pre.split(',');
+        preRequisites.map( (subject) => document.getElementById(subject).classList.remove('pre'));
+      }
+    },
+    data() {
+      return {
+        hovering: false
+      }
+    }
+}
+</script>
+
+<style scoped>
+table {
+    border: 1px solid #000;
+    border-collapse: collapse;
+    margin: 20px 5px;
+    cursor: pointer;    
+}
+
+tr, td {
+  border: 1px solid #000;    
+}
+
+.subject {
+  padding: 0px 30px;
+  width: 160px;
+  height: 60px;
+  max-width: 160px;  
+  max-height: 60px;
+  text-align: center;
+  box-sizing: border-box;    
+}
+
+.cr {
+  padding: 2px 10px;
+}
+
+.carga {
+  padding: 20px 10px 2px 10px;  
+}
+
+body {
+  font-family: arial;
+}
+
+.main {
+  background-color: #A2D3C2;
+}
+
+.pre {
+  background-color: #EAD637;
+}
+
+.oferta {
+  background-color: #D6F49D;
+}
+
+</style>
